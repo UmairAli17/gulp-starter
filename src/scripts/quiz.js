bbc6e822d1144e21d62449d60f045da95fbc0bfe
U.Ali U1351864 <U1351864@unimail.hud.ac.uk>
2016-10-20 14:47:08 +0100

    Sent values to session

--- src/scripts/quiz.js
+++ src/scripts/quiz.js
@@ -4,20 +4,26 @@ $(document).ready(function() {
 	var $currentQuestion = 0;
 	var $questions = $('.questions');
 	var $options = $('.options');
-
+    
+    
 	answers = new Object();
-		$options.change(function(){
+		$options.click(function(){
 		    var answer = ($(this).attr('value'))
 		    var question = ($(this).attr('name'))
 		    var proc_val = ($(this).data("proc-val"))
+            console.log(proc_val);
+            //declare the "quest" (question) variable
+            var quest;
+            //If the item exisions withint he questions class elelemnt then do the following
+            if(sessionStorage.getItem('question')){
+                //parse values to jSON
+                quest= JSON.parse(sessionStorage.getItem('question'));
+            }else{
+                quest=[];
+            }
+            //Send the value proc_val values to the jSOn array.
+            quest.push(proc_val)
+            sessionStorage.setItem('question', JSON.stringify(quest));
 		});
-
-	$options.each(function() {
-		$(this).on("click", function(){
-			$calc = $procScore + parseInt(answers[proc_val]);
-			console.log($calc);
-	    })
-	});
-
-
+    
 });
